c109990d0269468dc5f899a26779589e5d80c066
adandoang <akhdanjzif@gmail.com>
2024-12-18 10:53:58 +0700

    membuat ContainerApp.kt isinya InterfaceContainerApp dan didalamnya membuat 2 variabel, lalu membuat class ContainerApp

---
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -0,0 +1,22 @@
+package com.example.ucp2.dependenciesinjection
+
+import android.content.Context
+import com.example.ucp2.data.database.KrsDatabase
+import com.example.ucp2.repository.LocalRepositoryDosen
+import com.example.ucp2.repository.LocalRepositoryMK
+import com.example.ucp2.repository.RepositoryDosen
+import com.example.ucp2.repository.RepositoryMK
+
+interface InterfaceContainerApp {
+    val repositoryMK: RepositoryMK
+    val repositoryDsn: RepositoryDosen
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val repositoryDsn: RepositoryDosen by lazy {
+        LocalRepositoryDosen(KrsDatabase.getDatabase(context).dosenDao())
+    }
+    override val repositoryMK: RepositoryMK by lazy {
+        LocalRepositoryMK(KrsDatabase.getDatabase(context).mataKuliahDao())
+    }
+}
