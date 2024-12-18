0d68b6f404e6aaed60588a71baa16ddbc0309380
adandoang <akhdanjzif@gmail.com>
2024-12-18 10:46:57 +0700

    membuat LocalRepositoryMK

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryMK.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryMK.kt
@@ -1,4 +1,25 @@
 package com.example.ucp2.repository
 
-class LocalRepositoryMK {
+import com.example.ucp2.data.dao.MataKuliahDao
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMK (
+    private val mataKuliahDao: MataKuliahDao
+) : RepositoryMK{
+    override suspend fun insertMK(mataKuliah: MataKuliah) {
+        mataKuliahDao.insertMataKuliah(mataKuliah)
+    }
+    override suspend fun deleteMK(mataKuliah: MataKuliah) {
+        mataKuliahDao.deleteMataKuliah(mataKuliah)
+    }
+    override suspend fun updateMK(mataKuliah: MataKuliah) {
+        mataKuliahDao.updateMataKuliah(mataKuliah)
+    }
+    override fun getAllMK(): Flow<List<MataKuliah>> {
+        return mataKuliahDao.getAllMataKuliah()
+    }
+    override fun getMK(kode: String): Flow<MataKuliah> {
+        return mataKuliahDao.getMataKuliah(kode)
+    }
 }
