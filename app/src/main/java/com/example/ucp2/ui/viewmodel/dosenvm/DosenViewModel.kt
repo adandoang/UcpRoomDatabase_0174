6c87c78c8ee9ee7b44cc3fc29fbdc8d12cc48b2e
adandoang <akhdanjzif@gmail.com>
2024-12-18 11:13:57 +0700

    membuat DosenViewModel isinya operasi insert

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosenvm/DosenViewModel.kt
@@ -0,0 +1,95 @@
+package com.example.ucp2.ui.viewmodel.dosenvm
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.repository.RepositoryDosen
+import kotlinx.coroutines.launch
+
+//data class variabel yang menyimpan data input form
+data class DosenEvent(
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+)
+
+//Menyimpan input form ke dalam entity
+fun DosenEvent.toDosenEntity(): Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+)
+
+data class DsnUIState(
+    val DosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null
+)
+
+data class FormErrorState(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+){
+    fun isValid(): Boolean {
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
+
+class DosenViewModel(private val  repositoryDsn: RepositoryDosen) : ViewModel() {
+    var uiState by mutableStateOf(DsnUIState())
+
+    fun updateState(DosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            DosenEvent = DosenEvent,
+
+            )
+    }
+
+    private fun validateField(): Boolean {
+        val event = uiState.DosenEvent
+        val errorState = FormErrorState(
+            nidn = if (event.nidn.isNotEmpty()) null else "NIDN tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun saveData() {
+        val currentEvent = uiState.DosenEvent
+        if (validateField()) {
+            viewModelScope.launch {
+                try {
+                    repositoryDsn.insertDsn(currentEvent.toDosenEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Berhasil Disimpan",
+                        DosenEvent = DosenEvent(), //reset input form
+                        isEntryValid = FormErrorState() //reset error state
+                    )
+                } catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data Gagal Disimpan"
+                    )
+                }
+            }
+        } else {
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid, Periksa kembali data anda"
+            )
+        }
+    }
+
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
+
+
+
+
