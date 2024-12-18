efbc5898bbb85ab766c31f05c50bdf0befb59b71
adandoang <akhdanjzif@gmail.com>
2024-12-18 10:22:30 +0700

    membuat interface DosenDao yang berisi metod insert, getAllDosen, dan getDosen by nidn

---
+++ app/src/main/java/com/example/ucp2/data/dao/DosenDao.kt
@@ -0,0 +1,17 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+interface DosenDao {
+    @Insert
+    suspend fun insertDosen(dosen: Dosen)
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getDosen(nidn: String): Flow<Dosen>
+}
