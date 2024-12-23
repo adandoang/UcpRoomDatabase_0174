6e90bc542fa128a8bf97c69e484ee86c689f50fc
adandoang <akhdanjzif@gmail.com>
2024-12-23 09:40:08 +0700

    membuat detail viewmodel untuk penghubung view dari detail data dan modelny, beserta validasinya

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliahvm/DetailMataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliahvm/DetailMataKuliahViewModel.kt
@@ -1,2 +1,90 @@
 package com.example.ucp2.ui.viewmodel.matakuliahvm
 
+package com.example.ucp2.ui.viewmodel
+
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.LocalRepositoryMK
+import com.example.ucp2.repository.RepositoryMK
+import com.example.ucp2.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+data class DetailUiState(
+    val detailUiEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MataKuliahEvent()
+
+    val  isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MataKuliahEvent()
+}
+
+
+class DetailMKViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMK: RepositoryMK
+) : ViewModel() {
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiDetail.kode])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMK.getMK(_kode)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMK() {
+        detailUiState.value.detailUiEvent.toMataKuliahEntity().let {
+            viewModelScope.launch {
+                repositoryMK.deleteMK(it)
+            }
+        }
+    }
+}
+
+fun MataKuliah.toDetailUiEvent(): MataKuliahEvent {
+    return MataKuliahEvent(
+        kode = kode,
+        nama = nama,
+        sks = sks,
+        jenis = jenis,
+        semester = semester,
+        dosenPengampu = dosenPengampu
+    )
+}
