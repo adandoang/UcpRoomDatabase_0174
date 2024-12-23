5c89bae316de6727e9d6bc4c308d95d44a48af65
adandoang <akhdanjzif@gmail.com>
2024-12-23 09:37:50 +0700

    membuat update viewmodel untuk penghubung view dari update data dan modelny, beserta validasinya

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliahvm/HomeMKViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.ucp2.ui.viewmodel.matakuliahvm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositoryDosen
+import com.example.ucp2.repository.RepositoryMK
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+data class HomeUiState (
+    val listMK: List<MataKuliah> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+
+class HomeDsnViewModel (
+    private val repositoryMK: RepositoryMK
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMK.getAllMK()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMK = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliahvm/UpdateMataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliahvm/UpdateMataKuliahViewModel.kt
@@ -1,2 +1,89 @@
 package com.example.ucp2.ui.viewmodel.matakuliahvm
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositoryMK
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import java.text.Normalizer.Form
+
+class UpdateMKViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMK: RepositoryMK
+): ViewModel() {
+    var updateUIState by mutableStateOf(MKUIState())
+        private set
+
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiUpdate.KODE])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMK.getMK(_kode)
+                .filterNotNull()
+                .first()
+                .toUIStateMK()
+        }
+    }
+
+    fun updateState(MataKuliahEvent: MataKuliahEvent) {
+        updateUIState = updateUIState.copy(
+            MataKuliahEvent = MataKuliahEvent,
+        )
+    }
+
+    fun validateFields(): Boolean {
+        val event = updateUIState.MataKuliahEvent
+        val errorState = FormErrorState(
+            kode = if (event.kode.isNotEmpty()) null else "kode tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks.isNotEmpty()) null else "sks tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "jenis tidak boleh kosong",
+            semester = if (event.semester.isNotEmpty()) null else "semester tidak boleh kosong",
+            dosenPengampu = if (event.dosenPengampu.isNotEmpty()) null else "dosenPengampu tidak boleh kosong"
+        )
+
+        updateUIState = updateUIState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun updateData() {
+        val currentEvent = updateUIState.MataKuliahEvent
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryMK.updateMK(currentEvent.toMataKuliahEntity())
+                    updateUIState = updateUIState.copy(
+                        snackBarMessage = "Data berhasil diupdate",
+                        MataKuliahEvent = MataKuliahEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                    println("snackbar message diatur: ${updateUIState.
+                    snackBarMessage}")
+                } catch (e: Exception) {
+                    updateUIState = updateUIState.copy(
+                        snackBarMessage = "Data gagal diupdate"
+                    )
+                }
+            }
+        } else {
+            updateUIState = updateUIState.copy(
+                snackBarMessage = "Data gagal diupdate"
+            )
+        }
+    }
+
+    fun resetSnackBarMessage() {
+        updateUIState = updateUIState.copy(snackBarMessage = null)
+    }
+}
+
+fun MataKuliah.toUIStateMK(): MKUIState = MKUIState(
+    MataKuliahEvent = this.toDetailUiEvent(),
+)
