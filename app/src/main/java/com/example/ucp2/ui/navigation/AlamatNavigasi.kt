01c0f8e136e9591ef1d9dc31a8de648eeef9abd9
adandoang <akhdanjzif@gmail.com>
2024-12-23 10:02:17 +0700

    membuat alamatnavigasi dan memberikan route2nya untuk digunakan di pengelolahalaman

---
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.ui.navigation
+
+
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiHomeDsn : AlamatNavigasi {
+    override val route = "homedsn"
+}
+
+object DestinasiHomeMK : AlamatNavigasi {
+    override val route = "homemk"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
